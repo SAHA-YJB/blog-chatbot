ffa6d08334d903712ac57c11171897b926108de5
SAHA-YJB <pkomom123@naver.com>
2024-01-10 15:31:16 +0900

    edit: Link edit

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -32,10 +32,15 @@ const Sidebar: FC<SidebarProps> = ({ isOpen, close }) => {
         WEB DEV
       </Link>
       <div className="mt-10 flex items-center gap-4">
-        <Link href="https://s-aha-dev.tistory.com" target="_blank">
+        <Link
+          href="https://lumbar-emoji-10f.notion.site/1504565fccb94676973d3a336daa946f?pvs=4"
+          target="_blank"
+        >
+          <span className="sr-only">resume</span>
           <AiFillFileText className="h-6 w-6" />
         </Link>
         <Link href="https://github.com/SAHA-YJB" target="_blank">
+          <span className="sr-only">github</span>
           <AiFillGithub className="h-6 w-6" />
         </Link>
       </div>
