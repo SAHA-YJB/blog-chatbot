0c01b4b69bd02f12068182f2862fd83ed2f7d3a7
SAHA <pkomom123@naver.com>
2024-01-13 10:56:26 +0900

    fix: post API parser complete

--- src/pages/api/posts.ts
+++ src/pages/api/posts.ts
@@ -2,51 +2,50 @@ import { Post, PostRequest } from '@/types';
 import { createClient } from '@/utils/supabase/server';
 import type { StorageError } from '@supabase/storage-js';
 import formidable from 'formidable';
-import IncomingForm from 'formidable';
 import { readFileSync } from 'fs';
 import type { NextApiRequest, NextApiResponse } from 'next';
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Post | StorageError>,
 ) {
   try {
-    console.log('응답시작');
-    const form = IncomingForm();
+    const form = formidable();
     const [fields, files] = await form.parse(req);
-    console.log('응답시작2');
 
     let preview_image_url: string | null = null;
 
     const supabase = await createClient(req.cookies);
-    console.log('응답시작3');
+
     if (files.preview_image?.length === 1) {
       const file = files.preview_image[0];
       const fileContent = await readFileSync(file.filepath);
       const fileName = `${file.newFilename}_${file.originalFilename}`;
-      console.log('응답시작4');
+
       const { data: uploadData, error } = await supabase.storage
         .from('blog-image')
         .upload(fileName, fileContent, {
           contentType: file.mimetype ?? undefined,
         });
-      console.log('응답시작5');
       if (error) {
         res.status(403).json(error);
       }
       if (uploadData?.path) {
-        console.log('응답시작6');
         const { data } = await supabase.storage
           .from('blog-image')
           .getPublicUrl(uploadData.path);
         preview_image_url = data.publicUrl;
-        console.log('응답시작7');
         if (error) {
           res.status(402).json(error);
         }
       }
     }
-    console.log('응답시작8');
     const { title, content, category, tags } = fields;
     const postRequest = {
       title: title?.[0],
@@ -60,7 +59,6 @@ export default async function handler(
       .from('Post')
       .insert([postRequest])
       .select();
-    console.log('응답시작9');
     if (data && data.length === 1) {
       const { tags, ...rest } = data[0];
       res.status(200).json({ ...rest, tags: JSON.parse(tags) as string[] });
--- src/pages/write/index.tsx
+++ src/pages/write/index.tsx
@@ -35,11 +35,7 @@ export default function Write({
       if (fileRef.current?.files?.[0]) {
         formData.append('preview_image', fileRef.current.files[0]);
       }
-      console.log('포스트요청1');
-      const response = await axios.post('/api/posts', formData, {
-        timeout: 5000,
-      });
-      console.log('포스트요청2');
+      const response = await axios.post('/api/posts', formData);
       const { data } = response;
 
       if (data.id) {
