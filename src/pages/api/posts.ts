a9ad5c1e249078df87db357f4fc3a2a72cc15c11
SAHA <pkomom123@naver.com>
2024-01-15 21:19:33 +0900

    comment: comment

--- src/pages/api/posts.ts
+++ src/pages/api/posts.ts
@@ -1,7 +1,5 @@
 // 사용자로부터 제목, 내용, 카테고리, 태그, 미리보기 이미지를 받아서 블로그 포스트를 생성
 // 생성된 포스트 정보를 응답으로 반환
-// 사용자로부터 제목, 내용, 카테고리, 태그, 미리보기 이미지를 받아서 블로그 포스트를 생성
-// 생성된 포스트 정보를 응답으로 반환
 import { Post, PostRequest } from '@/types';
 import { createClient } from '@/utils/supabase/server';
 import type { StorageError } from '@supabase/storage-js';
@@ -25,46 +23,50 @@ export default async function handler(
     const form = formidable();
     const [fields, files] = await form.parse(req);
 
-  let preview_image_url: string | null = null;
+    let preview_image_url: string | null = null;
 
-  const supabase = await createClient(req.cookies);
+    const supabase = await createClient(req.cookies);
 
-  if (files.preview_image?.length === 1) {
-    const file = files.preview_image[0];
-    const fileContent = await readFileSync(file.filepath);
-    const fileName = `${file.newFilename}_${file.originalFilename}`;
+    if (files.preview_image?.length === 1) {
+      const file = files.preview_image[0];
+      const fileContent = await readFileSync(file.filepath);
+      const fileName = `${file.newFilename}_${file.originalFilename}`;
 
-    const { data: uploadData, error } = await supabase.storage
-      .from('blog-image')
-      .upload(fileName, fileContent, {
-        contentType: file.mimetype ?? undefined,
-      });
-    if (error) {
-      res.status(403).json(error);
-    }
-    if (uploadData?.path) {
-      const { data } = await supabase.storage
+      const { data: uploadData, error } = await supabase.storage
         .from('blog-image')
-        .getPublicUrl(uploadData.path);
-      preview_image_url = data.publicUrl;
+        .upload(fileName, fileContent, {
+          contentType: file.mimetype ?? undefined,
+        });
+      if (error) {
+        res.status(403).json(error);
+      }
+      if (uploadData?.path) {
+        const { data } = await supabase.storage
+          .from('blog-image')
+          .getPublicUrl(uploadData.path);
+        preview_image_url = data.publicUrl;
+      }
     }
-  }
 
-  const { title, content, category, tags } = fields;
-  const postRequest = {
-    title: title?.[0],
-    content: content?.[0],
-    category: category?.[0],
-    tags: tags?.[0],
-    preview_image_url,
-  } as PostRequest;
+    const { title, content, category, tags } = fields;
+    const postRequest = {
+      title: title?.[0],
+      content: content?.[0],
+      category: category?.[0],
+      tags: tags?.[0],
+      preview_image_url,
+    } as PostRequest;
 
-  const { data } = await supabase.from('Post').insert([postRequest]).select();
+    const { data } = await supabase.from('Post').insert([postRequest]).select();
 
-  if (data && data.length === 1) {
-    const { tags, ...rest } = data[0];
-    res.status(200).json({ ...rest, tags: JSON.parse(tags) as string[] });
-  } else {
+    if (data && data.length === 1) {
+      const { tags, ...rest } = data[0];
+      res.status(200).json({ ...rest, tags: JSON.parse(tags) as string[] });
+    } else {
+      res.status(500).end();
+    }
+  } catch (error) {
+    console.error(error);
     res.status(500).end();
   }
 }
