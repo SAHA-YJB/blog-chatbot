ee29f70e776b37eb7f721b6ba010a3c13f38ce14
SAHA <pkomom123@naver.com>
2024-02-04 19:59:09 +0900

    features: sideBar edit hits baddge apply

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,13 +3,25 @@ import Link from 'next/link';
 import React, { FC } from 'react';
 import { AiFillGithub, AiFillFileText, AiOutlineClose } from 'react-icons/ai';
 import IconButton from './IconButton';
+import { useQuery } from '@tanstack/react-query';
+import { createClient } from '@/utils/supabase/client';
 
 interface SidebarProps {
   isOpen: boolean;
   close: () => void;
 }
 
+const supabase = createClient();
+
 const Sidebar: FC<SidebarProps> = ({ isOpen, close }) => {
+  const { data: existingCategories } = useQuery({
+    queryKey: ['categories'],
+    queryFn: async () => {
+      const { data } = await supabase.from('Post').select('category');
+      return Array.from(new Set(data?.map((post) => post.category)));
+    },
+  });
+
   return (
     <div
       className={cn(
@@ -23,15 +35,22 @@ const Sidebar: FC<SidebarProps> = ({ isOpen, close }) => {
       <Link href="/" className="w-48 font-medium text-gray-600 hover:underline">
         홈
       </Link>
-      <Link href="/" className="w-48 font-medium text-gray-600 hover:underline">
-        태그
-      </Link>
       <Link
-        href="/category/web-devlopment"
+        href="/tags"
         className="w-48 font-medium text-gray-600 hover:underline"
       >
-        WEB DEV
+        태그
       </Link>
+      {existingCategories?.map((category) => (
+        <Link
+          key={category}
+          href={`/category/${category}`}
+          className="w-48 font-medium text-gray-600 hover:underline"
+        >
+          {category}
+        </Link>
+      ))}
+
       <div className="mt-10 flex items-center gap-4">
         <IconButton
           href="https://lumbar-emoji-10f.notion.site/1504565fccb94676973d3a336daa946f?pvs=4"
@@ -44,6 +63,11 @@ const Sidebar: FC<SidebarProps> = ({ isOpen, close }) => {
           target="_blank"
           component={Link}
           href="https://github.com/SAHA-YJB"
+        />
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          alt="방문자 배지"
+          src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fblod-chatbot.vercel.app&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=%EB%B0%A9%EB%AC%B8%EC%9E%90&edge_flat=false"
         />
       </div>
     </div>
