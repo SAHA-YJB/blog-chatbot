d310a344e0cb1cd8b534ac1ddd3ca25cb3e7d339
SAHA <pkomom123@naver.com>
2024-02-09 08:07:08 +0900

    comment: input component

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,18 +1,21 @@
 import { cn } from '@/utils/style';
 import { ComponentPropsWithoutRef, forwardRef } from 'react';
 
+// ComponentPropsWithoutRef<'input'>을 통해 HTMLInputElement의 모든 속성들을 포함하는 타입을 정의
 type InputProps = ComponentPropsWithoutRef<'input'>;
 
+// forwardRef를 사용하여 ref를 전달받을 수 있는 Input 컴포넌트를 정의
+// 이 컴포넌트는 InputProps 타입의 props를 받음
 const Input = forwardRef<HTMLInputElement, InputProps>(
   ({ className, ...rest }, ref) => {
     return (
       <input
-        ref={ref}
+        ref={ref} //ref를 전달함
         className={cn(
           'rounded-md border border-gray-300 p-2 transition-all hover:border-gray-400',
-          className,
+          className, // 사용자가 추가로 전달한 className이 있으면 추가
         )}
-        {...rest}
+        {...rest} // 사용자가 전달한 나머지 props를 모두 전달함
       />
     );
   },
