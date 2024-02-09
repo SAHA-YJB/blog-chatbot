a10ad321d14c2a3e0cf74b0c1c5012ad15a98196
SAHA <pkomom123@naver.com>
2024-02-09 20:06:00 +0900

    comment: btn component comment

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,15 +1,19 @@
 import { cn } from '@/utils/style';
 import React, { ComponentPropsWithoutRef, FC } from 'react';
 
+// ComponentPropsWithoutRef<'button'>을 사용하여
+// HTMLButtonElement의 모든 속성들을 포함하는 타입을 정의
 type ButtonProps = ComponentPropsWithoutRef<'button'>;
 
 const Button: FC<ButtonProps> = ({ className, children, ...rest }) => {
   return (
+    // button 요소를 반환 / className과 children을 전달
     <button
       className={cn(
         'w-full rounded-md bg-gray-800 py-2 text-white transition-all hover:bg-gray-900',
         className,
       )}
+      {...rest}
     >
       {children}
     </button>
