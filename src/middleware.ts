a6fc6d27f90d257c78ea7cf7657c5628239364d5
SAHA <pkomom123@naver.com>
2024-02-09 20:39:47 +0900

    comment: hook middleware comment

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,5 @@
-// 미들웨어는 모든 요청을 한번 거쳐가는 작업을 할 수 있게 해준다.
+// 미들웨어는 모든 요청이 서버에 도달하기 전에 처리되는 함수
+// 이를 통해 요청을 검사하거나 수정하거나, 특정 작업을 수행
 import { NextRequest, NextResponse } from 'next/server';
 import React from 'react';
 import { createClient } from './utils/supabase/middleware';
@@ -6,10 +7,12 @@ import { createClient } from './utils/supabase/middleware';
 export async function middleware(request: NextRequest) {
   const { supabase, response } = createClient(request);
 
+  // 현재 유저 정보를 가져옴
   const {
     data: { user },
   } = await supabase.auth.getUser();
 
+  // 사용자의 역할이 'authenticated'가 아니라면 '/admin' 페이지로 리다이렉션
   if (user?.role !== 'authenticated') {
     return NextResponse.redirect(new URL('/admin', request.url));
   }
--- src/utils/hook.ts
+++ src/utils/hook.ts
@@ -2,11 +2,15 @@ import { useQuery } from '@tanstack/react-query';
 import { createClient } from './supabase/client';
 const supabase = createClient();
 
+// useCategories라는 custom hook을 정의
+// 블로그 포스트의 모든 카테고리를 데이터베이스에서 가져옴
 export const useCategories = () =>
   useQuery({
     queryKey: ['categories'],
     queryFn: async () => {
+      // 'Post' 테이블에서 'category' 컬럼의 데이터를 모두 가져옴
       const { data } = await supabase.from('Post').select('category');
+      // 중복된 카테고리를 제거하고, 배열로 변환하여 반환
       return Array.from(new Set(data?.map((post) => post.category)));
     },
   });
@@ -16,6 +20,7 @@ export const useTags = () =>
     queryKey: ['tags'],
     queryFn: async () => {
       const { data } = await supabase.from('Post').select('tags');
+      // 각 post의 태그 배열을 하나의 배열로 합치고, 중복된 태그를 제거한 후 배열로 변환하여 반환
       return Array.from(
         new Set(data?.flatMap((post) => JSON.parse(post.tags))),
       );
