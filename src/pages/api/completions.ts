b52e1e3a03218b09ed8591bd8ff49692deb7e82b
SAHA <pkomom123@naver.com>
2024-02-13 22:39:45 +0900

    comment: openAI api comment

--- src/pages/api/completions.ts
+++ src/pages/api/completions.ts
@@ -1,12 +1,10 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import OpenAi from 'openai';
-import {
-  ChatCompletionMessageParam,
-  ChatCompletionMessage,
-} from 'openai/resources/index.mjs';
+import { ChatCompletionMessage } from 'openai/resources/index.mjs';
 
 const openai = new OpenAi({ apiKey: process.env.OPENAI_API_KEY });
 
+// API 응답의 타입을 정의
 type CompletionsResponse = {
   messages: ChatCompletionMessage[];
 };
@@ -15,13 +13,16 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<CompletionsResponse>,
 ) {
+  // 요청 메서드가 GET이 아니라면 405(Method Not Allowed) 상태 코드를 반환하고 함수를 종료
   if (req.method !== 'GET') {
     res.status(405).end();
     return;
   }
 
+  // 메시지를 저장할 배열을 생성
   const messages: ChatCompletionMessage[] = [];
 
+  // OpenAI API를 이용하여 대화를 생성합
   const response = await openai.chat.completions.create({
     messages: [
       {
@@ -29,6 +30,7 @@ export default async function handler(
         content: '다 알고 있는 너는 나만의 챗봇',
       },
     ],
+    // 사용할 모델을 지정
     model: 'gpt-4-0613',
   });
 
