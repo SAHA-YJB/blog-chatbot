8a0728def49048ddbd0ec579de2124b6e91f36a6
SAHA <pkomom123@naver.com>
2024-02-29 23:56:06 +0900

    comment: comment

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -15,6 +15,8 @@ const inter = Inter({ subsets: ['latin'] });
 export default function App({ Component, pageProps }: AppProps) {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   return (
+    // QueryClientProvider로 감싸주어야함
+    // 전체적인 레이아웃에 쿼리 클라이언트를 제공
     <QueryClientProvider client={queryClient}>
       <div
         className={cn('flex h-screen w-screen lg:text-base', inter.className)}
